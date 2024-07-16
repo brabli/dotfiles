4730a22304d06dd47e45753a73e40c0761fc32f2
brabli <67018167+brabli@users.noreply.github.com>
2024-07-16 20:52:42 +0100

    Add some oil keybinds to get hjkl working as normal again

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,14 @@
 return {
     "stevearc/oil.nvim",
     opts = {
+        -- columns = { "icon" },
+        keymaps = {
+            ["<C-h>"] = false,
+            ["<C-l>"] = false,
+            ["<C-k>"] = false,
+            ["<C-j>"] = false,
+            -- ["<M-h>"] = "actions.select_split",
+        },
         view_options = {
             show_hidden = true,
         },
