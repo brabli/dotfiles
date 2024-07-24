4d1234ffce791a51b34bf7c3cbce0c6164435c54
brabli <67018167+Brabli@users.noreply.github.com>
2024-07-25 00:25:54 +0100

    Install leap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -160,5 +160,7 @@ require("lazy").setup("plugins", {
     -- other settings
 })
 
+require("leap").create_default_mappings()
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
