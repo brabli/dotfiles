7d0681f7d175a020390268c8f56e89fafc26533c
brabli <67018167+Brabli@users.noreply.github.com>
2024-07-30 01:06:48 +0100

    Add si keybinding to search ignored files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -77,6 +77,13 @@ return { -- Fuzzy Finder (files, lsp, etc)
         vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
         vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
         vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+        -- Add a way to search ignored files
+        vim.keymap.set(
+            "n",
+            "<leader>si",
+            [[<cmd>lua require('telescope.builtin').find_files({ hidden = true, no_ignore = true })<CR>]],
+            { noremap = true, silent = true, desc = "[S]earch [I]gnored" }
+        )
 
         -- Slightly advanced example of overriding default behavior and theme
         vim.keymap.set("n", "<leader>/", function()
