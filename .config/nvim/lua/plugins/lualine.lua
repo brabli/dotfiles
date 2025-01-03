459576da5a35d0b72315b11c99eb063af67a9cc7
brabli <67018167+brabli@users.noreply.github.com>
2025-01-03 16:30:38 +0000

    Update lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,18 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local lualine = require("lualine")
-        lualine.setup({
-            options = {
-                -- theme = "gruvbox",
-            },
-        })
-    end,
+   "nvim-lualine/lualine.nvim",
+   dependencies = { "nvim-tree/nvim-web-devicons" },
+   config = function()
+      local lualine = require("lualine")
+      lualine.setup({
+         options = { section_separators = "", component_separators = "" },
+         sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename" },
+            lualine_x = { "filetype" },
+            -- lualine_y = {},
+            lualine_z = { "location" },
+         },
+      })
+   end,
 }
