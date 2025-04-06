b55c8f00fffa3e59aab86df4cc42aa371e91cab2
brabli <67018167+brabli@users.noreply.github.com>
2025-04-06 23:06:28 +0100

    Remove deprecated methods

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -111,9 +111,15 @@ vim.opt.scrolloff = 20
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+local jump = function(count)
+   return function()
+      vim.diagnostic.jump({ count = count, float = true })
+   end
+end
+
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "]d", jump(1), { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "[d", jump(-1), { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
