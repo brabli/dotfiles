1034a5be34386503372eb5ddaa436f0620161408
brabli <67018167+brabli@users.noreply.github.com>
2025-05-22 20:53:04 +0100

    Remove some unneeded bits

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -155,21 +155,13 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-         -- clangd = {},
-         -- gopls = {},
-         -- pyright = {},
          -- rust_analyzer = {},
          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
          --
          -- Some languages (like typescript) have entire language plugins that can be useful:
          --    https://github.com/pmizio/typescript-tools.nvim
-         --
-         -- But for many setups, the LSP (`tsserver`) will work just fine
-         -- tsserver = {},
-         --
          intelephense = {
             -- To add license you need a file called `~/intelephense/licence.txt` which contains your license.
-
             settings = {
                intelephense = {
                   files = {
@@ -181,14 +173,12 @@ return {
 
          emmet_language_server = {
             filetypes = {
-               "css",
-               "eruby",
                "html",
-               "javascript",
+               "twig",
+               "css",
                "less",
                "sass",
                "scss",
-               "twig",
             },
          },
 
