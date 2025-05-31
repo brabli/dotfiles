1ab9c303c9b8e3f6f55e34efbd17773583befc47
brabli <67018167+brabli@users.noreply.github.com>
2025-05-31 23:31:09 +0100

    Add settings to rust-analyzer

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -15,41 +15,9 @@ return {
       { "folke/neodev.nvim", opts = {} },
    },
    config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
       vim.api.nvim_create_autocmd("LspAttach", {
          group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
          callback = function(event)
-            -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-            -- to define small helper and utility functions so you don't have to repeat yourself.
-            --
             -- In this case, we create a function that lets us more easily define mappings specific
             -- for LSP related items. It sets the mode, buffer and description for us each time.
             local map = function(keys, func, desc)
@@ -103,6 +71,7 @@ return {
             --
             -- When you move your cursor, the highlights will be cleared (the second autocommand).
             local client = vim.lsp.get_client_by_id(event.data.client_id)
+
             if client and client.server_capabilities.documentHighlightProvider then
                local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
                vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -128,8 +97,6 @@ return {
 
             -- The following autocommand is used to enable inlay hints in your
             -- code, if the language server you are using supports them
-            --
-            -- This may be unwanted, since they displace some of your code
             if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
                map("<leader>th", function()
                   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
@@ -155,13 +122,14 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-         -- rust_analyzer = {},
          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
          --
          -- Some languages (like typescript) have entire language plugins that can be useful:
          --    https://github.com/pmizio/typescript-tools.nvim
+         -- To add license you need a file called `~/intelephense/licence.txt` which contains your license.
+
+         -- BG: These don't fucking work anymore, keeping them here so I can rage about them some more
          intelephense = {
-            -- To add license you need a file called `~/intelephense/licence.txt` which contains your license.
             settings = {
                intelephense = {
                   files = {
@@ -182,12 +150,7 @@ return {
             },
          },
 
-         tailwindcss = {},
-
          lua_ls = {
-            -- cmd = {...},
-            -- filetypes = { ...},
-            -- capabilities = {},
             settings = {
                Lua = {
                   completion = {
@@ -216,7 +179,7 @@ return {
          "biome",
          "php-cs-fixer",
          "tailwindcss",
-         "rust-analyzer",
+         "rust_analyzer",
          "bashls",
          "mdx_analyzer",
          "shellharden",
@@ -231,14 +194,17 @@ return {
       })
 
       require("mason-lspconfig").setup({
-         ensure_installed = {},
-         automatic_installation = {},
+         ensure_installed = {
+            "rust_analyzer",
+         },
+         automatic_installation = false,
          handlers = {
             function(server_name)
+               vim.notify("Help")
                local server = servers[server_name] or {}
                -- This handles overriding only values explicitly passed
                -- by the server configuration above. Useful when disabling
-               -- certain features of an LSP (for example, turning off formatting for tsserver)
+               -- certain features of an LSP (for example, turning off formatting for ts_ls)
                server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
                require("lspconfig")[server_name].setup(server)
             end,
@@ -248,5 +214,19 @@ return {
       -- Manually installed `just-lsp` on Arch as it wasn't installing via Mason for some reason
       local lsp = require("lspconfig")
       lsp.just.setup({})
+
+      -- Oh my God this took so fucking long to figure out how to do
+      -- https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#configuration
+      vim.lsp.config("rust_analyzer", {
+         settings = {
+            ["rust-analyzer"] = {
+               completion = {
+                  postfix = {
+                     enable = false,
+                  },
+               },
+            },
+         },
+      })
    end,
 }
