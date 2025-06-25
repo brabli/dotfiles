d62bb348b3a308ad9a4db5acde5e188c328f3b81
brabli <67018167+brabli@users.noreply.github.com>
2025-06-25 14:23:19 +0100

    Use o instead of opt

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,10 @@
 -- BG: Lua guide
 -- https://neovim.io/doc/user/lua-guide.html
 
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.tabstop = 4 -- A tab is 4 spaces
-vim.opt.softtabstop = 4 -- A tab key press inserts 4 spaces
-vim.opt.shiftwidth = 4 -- Indentation is 4 spaces
+vim.o.expandtab = true -- Use spaces instead of tabs
+vim.o.tabstop = 4 -- A tab is 4 spaces
+vim.o.softtabstop = 4 -- A tab key press inserts 4 spaces
+vim.o.shiftwidth = 4 -- Indentation is 4 spaces
 
 vim.api.nvim_create_autocmd("FileType", {
    pattern = "twig",
